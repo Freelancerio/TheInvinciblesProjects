31d7ec362838d60319e1b6fc392719d8fdcdbcbb
LuvoZulu <zululuvo03@gmail.com>
2025-08-31 16:41:43 +0200

    working project

--- Backend/src/main/java/com/smartbet/SmartBetApplication.java
+++ Backend/src/main/java/com/smartbet/SmartBetApplication.java
@@ -7,15 +7,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-<<<<<<< HEAD
 @EnableScheduling  // Enable scheduled tasks
 @EnableAsync       // Enable async execution
 @ComponentScan(basePackages = {"com.smartbet", "com.footballapi"}) // Scan both packages
-=======
-@EnableScheduling
-@EnableAsync     
-@ComponentScan(basePackages = {"com.smartbet", "com.footballapi"})
->>>>>>> 3f151e00f7c6f3d4897c3ce182d2aedf2c37a412
 public class SmartBetApplication {
 
     public static void main(String[] args) {
