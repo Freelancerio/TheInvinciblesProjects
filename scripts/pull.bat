8846c49915546308070ed2b7c30ec3f5198acd26
Luvo Zulu <160118100+LuvoZulu@users.noreply.github.com>
2025-09-02 17:01:22 +0200

    Delete scripts directory

--- scripts/pull.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd ..
-git checkout dev
-git pull origin dev --allow-unrelated-histories
--- scripts/pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ..
-git branch dev
-git checkout dev
-git pull origin dev --allow-unrelated-histories
-cd ..
