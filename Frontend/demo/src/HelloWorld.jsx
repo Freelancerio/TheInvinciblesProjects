31923494d6e3aa521f930a08363808f38c5e23fa
Stephan0709 <stephanbotha0709@gmail.com>
2025-09-03 20:14:48 +0200

    Delete Frontend directory

--- Frontend/demo/src/HelloWorld.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function HelloWorld() {
-  return <h1>Hello, World!</h1>;
-}
-
-export default HelloWorld;
-// can delete this file and HelloWorld.test.js just to make sure Jest works
