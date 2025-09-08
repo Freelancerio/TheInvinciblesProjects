8e0f53c50ab8c14fe92bff8f7452f1d1c7de963d
LuvoZulu <zululuvo03@gmail.com>
2025-09-08 22:09:31 +0200

    code integration with Stephan's code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Profile from "./pages/Profile.jsx";
 import Home from "./pages/Home.jsx";
+import { LandingPage } from "./pages/LandingPage.jsx";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,6 @@ import './styles/global.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
